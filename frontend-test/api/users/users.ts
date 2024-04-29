8e7095f156d4bc4e76ef36215a59ae3b2b4f1345
OliverSchlueter <OliverSchlueter@users.noreply.github.com>
2024-04-29 21:13:59 +0200

    frontend-test: add example

--- frontend-test/api/users/users.ts
+++ frontend-test/api/users/users.ts
@@ -1,10 +1,12 @@
 import {User} from "./types";
 
 export async function getUserById(id: string): Promise<User> {
-    const response = await fetch('http://localhost:8080/v1/users/' + id, {
+    const response = await fetch('http://localhost:8080/api/v1/users/' + id, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
+            'AuthUser': 'test@email.de',
+            'AuthPassword': '541fsd15ds4f15',
         }
     })
 
@@ -23,4 +25,36 @@ export async function getUserById(id: string): Promise<User> {
         followers: data.followers,
         following: data.following,
     }
+}
+
+export async function createUser(user: User): Promise<void> {
+    const response = await fetch('http://localhost:8080/api/v1/users/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user)
+    })
+
+    if (!response.ok || response.status !== 200) {
+        throw new Error('Failed to create user: ' + await response.text())
+    }
+}
+
+async function use() {
+    try {
+        await createUser({
+            id: '1',
+            username: 'test',
+            email: 'test@test.de',
+            password: 'test',
+            created_at: 0,
+            followers: [],
+            following: [],
+        });
+    } catch (e) {
+        window.alert('Failed to create user.')
+    }
+
+    // ...
 }
