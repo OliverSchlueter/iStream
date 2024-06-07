4f700e92bb1b66f52f8135d4f7f2addb72113eab
Eryk <kynast-e@web.de>
2024-06-07 13:07:59 +0200

    frontend: login & register routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {RegistrationComponent} from "../registration/registration.component";
+import {LoginComponent} from "../login/login.component";
 
 export const routes: Routes = [
-  {path: "register", component: RegistrationComponent}
+  {path: "register", component: RegistrationComponent},
+  {path: "login", component: LoginComponent},
 ];
