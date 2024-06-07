421aa7266d91304ebf79df4eb6ed4bee79eddfbb
Eryk <kynast-e@web.de>
2024-06-07 13:02:07 +0200

    fix registration, set route from header to registration

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {RegistrationComponent} from "../registration/registration.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "register", component: RegistrationComponent}
+];
