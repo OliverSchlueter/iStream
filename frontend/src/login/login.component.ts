17e81a047bed77cd62be6fda3e27fec3a26b9840
Eryk <kynast-e@web.de>
2024-06-10 12:01:33 +0200

    frontend: design LogIn page, login method

--- frontend/src/login/login.component.ts
+++ frontend/src/login/login.component.ts
@@ -8,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username: string = "";
+  password: string = "";
 
+  public async login(){
+    console.log(this.username, this.password)
+    const response = await fetch("http://localhost:8080/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username: this.username,
+        password: this.password
+      })
+    })
+
+    if (response.status === 201){
+      console.log("You're logged into your account")
+    } else {
+      console.error("User not found: "+ response.statusText)
+    }
+  }
 }
