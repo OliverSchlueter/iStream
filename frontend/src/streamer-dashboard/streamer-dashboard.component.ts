7d84eb302f9f7238a8dcf93f000bb109c102535b
Eryk <kynast-e@web.de>
2024-06-20 18:25:53 +0200

    frontend: add method: calculate Streamtime

--- frontend/src/streamer-dashboard/streamer-dashboard.component.ts
+++ frontend/src/streamer-dashboard/streamer-dashboard.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
-import {fetchOnlineStreamers, fetchUser, User} from "../api/streams";
+import {fetchOnlineStreamers, fetchStream, fetchUser, Stream, User} from "../api/streams";
+import {min} from "rxjs";
 
 @Component({
   selector: 'app-streamer-dashboard',
@@ -11,10 +12,35 @@ import {fetchOnlineStreamers, fetchUser, User} from "../api/streams";
 export class StreamerDashboardComponent {
 
   user: User | null = null;
+  stream: Stream | null = null;
 
   constructor() {
     fetchUser(localStorage.getItem('username')!).then((user) => {
       this.user = user
     })
+    fetchStream(localStorage.getItem('username')!).then((stream) => {
+      this.stream = stream
+    })
+  }
+
+  public calculateStreamTime(ms: number) {
+    let secs = 0
+    let mins = 0
+    let hours = 0
+
+    while (ms > 1000) {
+      secs++
+      ms -= 100
+    }
+    while (secs > 60) {
+      mins++
+      secs -= 60
+    }
+    while (mins > 60) {
+      hours++
+      mins -= 100
+    }
+
+    return Date.now() -  hours + ":" + mins + ":" + secs
   }
 }
