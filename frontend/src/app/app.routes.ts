778f3a24f5e07b41378ff9ae17b3c2e9bdedbea5
Privat <eryk.kynast@studmail.w-hs.de>
2024-06-20 11:47:49 +0200

    Frontend: begin previeStream

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import {RegistrationComponent} from "../registration/registration.component";
 import {LoginComponent} from "../login/login.component";
 import { WatchviewComponent } from '../watchview/watchview.component';
 import {LandingpageComponent} from "../landingpage/landingpage.component";
+import {StreamerDashboardComponent} from "../streamer-dashboard/streamer-dashboard.component";
 
 export const routes: Routes = [
   {path: "register", component: RegistrationComponent},
   {path: "login", component: LoginComponent},
   {path: "watchview", component: WatchviewComponent},
+  {path: "streamerDashboard", component: StreamerDashboardComponent},
   {path: "", component: LandingpageComponent},
 ];
