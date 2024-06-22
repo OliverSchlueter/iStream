ac3395328888f2d0c80be34ada4873e22c93efa0
Eryk <kynast-e@web.de>
2024-06-22 13:12:11 +0200

    frontend: initial livestream-component

---
+++ frontend/src/livestream/livestream.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-livestream',
+  standalone: true,
+  imports: [],
+  templateUrl: './livestream.component.html',
+  styleUrl: './livestream.component.css'
+})
+export class LivestreamComponent {
+
+}
