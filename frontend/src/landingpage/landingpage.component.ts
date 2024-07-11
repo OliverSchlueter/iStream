a0029b2ce6fa5ac87c3cc8322a4dee09cdcd851f
OliverSchlueter <OliverSchlueter@users.noreply.github.com>
2024-07-11 18:10:08 +0200

    frontend: add streamnamer to dummys

--- frontend/src/landingpage/landingpage.component.ts
+++ frontend/src/landingpage/landingpage.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {NgFor, NgForOf} from "@angular/common";
-import { Stream, fetchOnlineStreamers, fetchUser } from '../api/streams';
+import {fetchOnlineStreamers, fetchUser, Stream} from '../api/streams';
 
 
 @Component({
@@ -13,45 +13,136 @@ import { Stream, fetchOnlineStreamers, fetchUser } from '../api/streams';
 })
 export class LandingpageComponent {
   StreamerHome: Stream[] | null = [
-    {streamer: "Trymacs", amountViewers:18.574, liveSince:0, streamConfig: {title:"Hallo", userId:"", description:"", category:""}},
-    {streamer: "ELoTRIX", amountViewers:3.741, liveSince:0, streamConfig: {title:"Hallo", userId:"", description:"", category:""}},
-    {streamer: "Staiy", amountViewers:6.735, liveSince:0, streamConfig: {title:"Hallo", userId:"", description:"", category:""}},
-    {streamer: "rewinside", amountViewers:2.516, liveSince:0, streamConfig: {title:"Hallo", userId:"", description:"", category:""}},
-    {streamer: "ApoRed", amountViewers:7, liveSince:0, streamConfig: {title:"Hallo", userId:"", description:"", category:""}},
-    {streamer: "KuchenTV", amountViewers:4.236, liveSince:0, streamConfig: {title:"Hallo", userId:"", description:"", category:""}},
-    {streamer: "GronkhTV", amountViewers:6.418, liveSince:0, streamConfig: {title:"Hallo", userId:"", description:"", category:""}},
-    {streamer: "AnniTheDuck", amountViewers:3, liveSince:0, streamConfig: {title:"Hallo", userId:"", description:"", category:""}},
-    {streamer: "HandOfBlood", amountViewers:7.489, liveSince:0, streamConfig: {title:"Hallo", userId:"", description:"", category:""}},
-    {streamer: "TheRealKnossi", amountViewers:13.116, liveSince:0, streamConfig: {title:"Hallo", userId:"", description:"", category:""}},
-    {streamer: "EliasN97", amountViewers:21.698, liveSince:0, streamConfig: {title:"Hallo", userId:"", description:"", category:""}},
-    {streamer: "BastiGHG", amountViewers:5.842, liveSince:0, streamConfig: {title:"Hallo", userId:"", description:"", category:""}},
-    {streamer: "ungespielt", amountViewers:4, liveSince:0, streamConfig: {title:"Hallo", userId:"", description:"", category:""}},
-    {streamer: "ARD", amountViewers:10.542, liveSince:0, streamConfig: {title:"Hallo", userId:"", description:"", category:""}},
-    {streamer: "Trymacs", amountViewers:19.124, liveSince:0, streamConfig: {title:"Hallo", userId:"", description:"", category:""}},
-      ];
+    {
+      streamerName: "Trymacs",
+      streamer: "Trymacs",
+      amountViewers: 18.574,
+      liveSince: 0,
+      streamConfig: {title: "Hallo", userId: "", description: "", category: ""}
+    },
+    {
+      streamerName: "ELoTRIX",
+      streamer: "ELoTRIX",
+      amountViewers: 3.741,
+      liveSince: 0,
+      streamConfig: {title: "Hallo", userId: "", description: "", category: ""}
+    },
+    {
+      streamerName: "Staiy",
+      streamer: "Staiy",
+      amountViewers: 6.735,
+      liveSince: 0,
+      streamConfig: {title: "Hallo", userId: "", description: "", category: ""}
+    },
+    {
+      streamerName: "rewinside",
+      streamer: "rewinside",
+      amountViewers: 2.516,
+      liveSince: 0,
+      streamConfig: {title: "Hallo", userId: "", description: "", category: ""}
+    },
+    {
+      streamerName: "ApoRed",
+      streamer: "ApoRed",
+      amountViewers: 7,
+      liveSince: 0,
+      streamConfig: {title: "Hallo", userId: "", description: "", category: ""}
+    },
+    {
+      streamerName: "KuchenTV",
+      streamer: "KuchenTV",
+      amountViewers: 4.236,
+      liveSince: 0,
+      streamConfig: {title: "Hallo", userId: "", description: "", category: ""}
+    },
+    {
+      streamerName: "GronkhTV",
+      streamer: "GronkhTV",
+      amountViewers: 6.418,
+      liveSince: 0,
+      streamConfig: {title: "Hallo", userId: "", description: "", category: ""}
+    },
+    {
+      streamerName: "AnniTheDuck",
+      streamer: "AnniTheDuck",
+      amountViewers: 3,
+      liveSince: 0,
+      streamConfig: {title: "Hallo", userId: "", description: "", category: ""}
+    },
+    {
+      streamerName: "HandOfBlood",
+      streamer: "HandOfBlood",
+      amountViewers: 7.489,
+      liveSince: 0,
+      streamConfig: {title: "Hallo", userId: "", description: "", category: ""}
+    },
+    {
+      streamerName: "TheRealKnossi",
+      streamer: "TheRealKnossi",
+      amountViewers: 13.116,
+      liveSince: 0,
+      streamConfig: {title: "Hallo", userId: "", description: "", category: ""}
+    },
+    {
+      streamerName: "EliasN97",
+      streamer: "EliasN97",
+      amountViewers: 21.698,
+      liveSince: 0,
+      streamConfig: {title: "Hallo", userId: "", description: "", category: ""}
+    },
+    {
+      streamerName: "BastiGHG",
+      streamer: "BastiGHG",
+      amountViewers: 5.842,
+      liveSince: 0,
+      streamConfig: {title: "Hallo", userId: "", description: "", category: ""}
+    },
+    {
+      streamerName: "ungespielt",
+      streamer: "ungespielt",
+      amountViewers: 4,
+      liveSince: 0,
+      streamConfig: {title: "Hallo", userId: "", description: "", category: ""}
+    },
+    {
+      streamerName: "ARD",
+      streamer: "ARD",
+      amountViewers: 10.542,
+      liveSince: 0,
+      streamConfig: {title: "Hallo", userId: "", description: "", category: ""}
+    },
+    {
+      streamerName: "Trymacs",
+      streamer: "Trymacs",
+      amountViewers: 19.124,
+      liveSince: 0,
+      streamConfig: {title: "Hallo", userId: "", description: "", category: ""}
+    },
+  ];
 
-      streamer: Stream[] | null = [];
-      constructor() {
-        fetchOnlineStreamers().then((streamers) => {
-            this.streamer = streamers;
+  streamer: Stream[] | null = [];
 
-            for (const s of streamers!) {
-              fetchUser(s.streamer).then((user) => {
-                if (user) {
-                  s.streamerName = user.username;
-                }
-              });
-            }
+  constructor() {
+    fetchOnlineStreamers().then((streamers) => {
+      this.streamer = streamers;
 
-            this.streamer!.push(...this.StreamerHome!);
+      for (const s of streamers!) {
+        fetchUser(s.streamer).then((user) => {
+          if (user) {
+            s.streamerName = user.username;
+          }
         });
-    }
-    
-      public streamerlink(s: any) {
-        window.postMessage({
-          type: "watch", 
-          streamer: s
-        })
-        console.log("hallo")
       }
+
+      this.streamer!.push(...this.StreamerHome!);
+    });
+  }
+
+  public streamerlink(s: any) {
+    window.postMessage({
+      type: "watch",
+      streamer: s
+    })
+    console.log("hallo")
+  }
 }
